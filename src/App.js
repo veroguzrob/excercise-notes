787bb16cd1dab71f7926ec915e96c9c9b9b662fb
veroguzrob72 <vgrdesd@gmail.com>
2022-05-27 19:54:27 -0500

    Se agrega un form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Ejercicio para trabajar con React</h1>
+      <h1>Ejercicio para trabajar con React</h1>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
